c8539f3c7c9dd4701f0c063aedd67fb7d6701da4
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2023-06-22 14:53:21 -0500

    Temp

--- main.rb
+++ main.rb
@@ -6,9 +6,9 @@ def print_each(a, b, n, actual)
   t_err = (t - actual).abs
   puts "------------------"
   puts "N = #{n}"
-  # puts "m: #{m.round(4)}"
+  puts "m: #{m.round(4)}"
   puts "t: #{t.round(4)}"
-  # puts "m_err: #{m_err.round(4)}"
+  puts "m_err: #{m_err.round(4)}"
   puts "t_err: #{t_err.round(4)}"
 end
 
@@ -54,31 +54,36 @@ end
 
 def trapezoid(a, b, n)
   delta_x = (b-a)/n
+  # puts "delta_x: #{delta_x}"
   xs = calc_trapezoid_xs(a, b, delta_x)
+  # puts "xs: #{xs.inspect}"
   acc = 0
   i = 1
-  while i < xs.size-2
+  while i < xs.size-1
     value = f(xs[i])
     acc += value
+    # puts "value for #{xs[i]}: #{value}"
     i += 1
   end
   first = f(xs.first)
+  # puts "value for #{xs.first}: #{value}"
   last = f(xs.last)
+  # puts "value for #{xs.last}: #{value}"
   (1.0/2.0)*delta_x*(first+(2*acc)+last)
 end
 
 def f(x)
-  4.0*Math.exp(-2.0*x)
+  5.0*Math.exp(-2.0*x)
 end
 
 def main
   a = 0.0
   b = 2.0
-  actual = 1.9634
+  actual = 2.4542
   print_each(a, b, 4.0, actual)
-  # print_each(a, b, 8.0, actual)
-  # print_each(a, b, 16.0, actual)
-  # print_each(a, b, 32.0, actual)
+  print_each(a, b, 8.0, actual)
+  print_each(a, b, 16.0, actual)
+  print_each(a, b, 32.0, actual)
 end
 
 main
